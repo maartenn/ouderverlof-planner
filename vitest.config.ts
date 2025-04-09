1aa5d60d5b7648e51385cd62717ea88edced10bb
maartenn <mnaberink@gmail.com>
2025-04-09 19:07:06 +0000

    set base url

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+const base = process.env.GITHUB_PAGES  ? '/ouderverlof-planner/' : '/';
 
 export default defineConfig({
   plugins: [react()],
+  base: base,
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
