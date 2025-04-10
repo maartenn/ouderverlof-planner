271e10e1b06e7c150a893b5d5ccd832c379d3c38
maartenn <mnaberink@gmail.com>
2025-04-10 09:03:37 +0200

    change baseurl in correct file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+const base = process.env.GITHUB_PAGES  ? '/ouderverlof-planner/' : '/';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-const base = process.env.GITHUB_PAGES  ? '/ouderverlof-planner/' : '/';
 
 export default defineConfig({
   plugins: [react()],
-  base: base,
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
